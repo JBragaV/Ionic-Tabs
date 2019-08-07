12f8930daa2f23fa909512c154ba14ed3eda2467
JBragaV <44869203+JBragaV@users.noreply.github.com>
2019-08-07 10:11:16 -0300

    aula07/08

--- IonicTab/src/app/form/form.page.ts
+++ IonicTab/src/app/form/form.page.ts
@@ -11,5 +11,4 @@ export class FormPage implements OnInit {
 
   ngOnInit() {
   }
-
 }
---
+++ IonicTab/src/app/models/profissional.models.ts
@@ -0,0 +1,7 @@
+export interface profissional{
+    nome: string,
+    email: string,
+    endereco: string,
+    senha: string,
+    tipoServico: string
+}
