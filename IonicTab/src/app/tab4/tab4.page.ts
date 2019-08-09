7e6cb56a263abf290f5b11f5ccf61959e455e56d
JBragaV <44869203+JBragaV@users.noreply.github.com>
2019-08-09 12:00:10 -0300

    Update da Aula09/08

--- IonicTab/src/app/tab4/tab4.page.ts
+++ IonicTab/src/app/tab4/tab4.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ClienteService } from '../service/cliente.service';
+import { cliente } from '../models/cliente.models';
 
 
 @Component({
@@ -19,21 +22,35 @@ export class Tab4Page implements OnInit {
   errorEndereco = false;
   errorPass = false;
 
-  constructor(private formBilder:FormBuilder) { }
+  constructor(private formBilder:FormBuilder, 
+              private clienteService: ClienteService,
+              private arota: Router
+    ) { }
 
   ngOnInit():void {
     this.formulario =this.formBilder.group({
-      "nome":["", Validators.required],
-      "email":["", [Validators.required]],
-      "endereco":["", Validators.required],
-      "password":["", Validators.compose([
+      nome:["o", [Validators.required, Validators.maxLength(35), Validators.minLength(2)]],
+      email:["i", [Validators.required, Validators.email, Validators.maxLength(40)]],
+      endereco:["p", [Validators.required, Validators.maxLength(100)]],
+      password:["m", Validators.compose([
           Validators.minLength(4), 
           Validators.maxLength(20), 
           Validators.required
         ])]
-    })
+    }, {updateOn: 'blur'})
   }
+  
+  add(){
+    //enviar para os serviços.
+    /*Resgatando os valores dos campos e fazendo i, cast(conversão)para o modelo)template Cliente*/
+    const novoCliente = this.formulario.getRawValue() as cliente;
 
+    this.clienteService.addCliente(novoCliente).subscribe(() => this.arota.navigateByUrl("ainda não sei"),
+                                  error => {
+                                    console.log(error);
+                                    this.formulario.reset();
+                                  })
+  }
   logar(){
     console.log(this.formulario.value)
     let {nome, email, endereco, password} = this.formulario.controls;
@@ -61,7 +78,7 @@ export class Tab4Page implements OnInit {
         this.msgPass = "Insira uma senha entre 4 e 20 caracteres!"
       }
     }else{
-      alert("Os dados seram enviados para o banco de dados!!!")
+      this.add();
     }
   }
 }
