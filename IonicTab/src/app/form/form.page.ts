1d28efb9fb84631a04f56ac56fe6eb37cc23e5d2
JBragaV <44869203+JBragaV@users.noreply.github.com>
2019-08-11 23:46:42 -0300

    novo

--- IonicTab/src/app/form/form.page.ts
+++ IonicTab/src/app/form/form.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-form',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormPage implements OnInit {
 
-  constructor() { }
+  constructor(private alertController: AlertController) { }
 
   ngOnInit() {
   }
 
+  async apareceae(){
+    const alert = await this.alertController.create({
+      header: "Alerta",
+      subHeader: "Sub título da bagaça",
+      message: "Esse alerta é um teste. Será que stou aprendendo mais IONIC!?",
+      buttons: ["OK"]
+    })
+    alert.present();
+  }
 }
